75eac7029a3a37dbcc29af0ed94d41b00682d032
Leomtz03 <79104199+Leomtz03@users.noreply.github.com>
2021-11-07 22:17:01 -0800

    Milestone 4, CST150

---
+++ Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milestone1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            idTextBox.Text = Form1.SetValueForID.ToString();
+            medicationTextBox.Text = Form1.SetValueForMedication.ToString();
+            formTextBox.Text = Form1.SetValueForDeliveryForm.ToString();
+            quantityTextBox.Text = Form1.SetValueForQuantity.ToString();
+            unitPriceTextBox.Text = Form1.SetValueForUnitPrice.ToString();
+        }
+    }
+}
